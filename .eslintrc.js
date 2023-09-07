4e2b0b8ee220dc9355759a6b920527fa5d33d467
Juanra GM <juanrgm724@gmail.com>
2023-09-07 09:49:33 +0200

    chore: relax eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,5 +7,10 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   rules: {
     "@typescript-eslint/no-floating-promises": ["error"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-var-requires": "off",
+    "@typescript-eslint/ban-types": "off",
+    "no-empty": "off",
   },
 };
