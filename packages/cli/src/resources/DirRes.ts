e154ad9e7e00cc9755d9e2569bdf9bab14219eb1
Juanra GM <juanrgm724@gmail.com>
2023-09-08 13:31:43 +0200

    refactor(cli): reuse method

--- packages/cli/src/resources/DirRes.ts
+++ packages/cli/src/resources/DirRes.ts
@@ -4,19 +4,19 @@ import { Writable } from "ts-essentials";
 
 export class DirRes<
   T extends Callable<Record<string, unknown> | unknown[] | undefined, any[]>,
-  TConf extends Record<string, unknown> = {}
+  TConf extends Record<string, unknown> = {},
 > extends AbstractRes<T, TConf> {
   protected static _tplResType = ResType.Dir;
   toString(): string {
     throw new Error("Not implemented");
   }
   add<ST extends Record<string, unknown>>(
-    data: T extends (...args: any[]) => any ? never : ST
+    data: T extends (...args: any[]) => any ? never : ST,
   ): DirRes<
     T extends () => any ? any : T extends Record<string, unknown> ? T & ST : ST
   >;
   add<ST extends unknown[]>(
-    data: T extends (...args: any[]) => any ? never : ST
+    data: T extends (...args: any[]) => any ? never : ST,
   ): DirRes<
     T extends () => any
       ? any
@@ -25,26 +25,20 @@ export class DirRes<
       : ST
   >;
   add<ST extends Record<string, unknown> | unknown[]>(
-    data: ST
+    data: ST,
   ): DirRes<T & ST> {
-    const self = this as Writable<typeof this>;
-    if (Array.isArray(data)) {
-      if (Array.isArray(self.data)) {
-        self.data = [...self.data, ...data] as any;
-      } else {
-        self.data = [data] as any;
-      }
-    } else {
-      if (Array.isArray(self.data)) {
-        self.data = data as any;
-      } else {
-        self.data = { ...self.data, ...data } as any;
-      }
-    }
-    return this as any as DirRes<T & ST>;
+    return this.set(
+      Array.isArray(data)
+        ? Array.isArray(this.data)
+          ? [...this.data, ...data]
+          : [data]
+        : Array.isArray(this.data)
+        ? data
+        : { ...this.data, ...data },
+    ) as any;
   }
   set<TData extends Record<string, unknown> | unknown[]>(
-    data: TData
+    data: TData,
   ): DirRes<TData> {
     const self = this as Writable<typeof this>;
     self.data = self.options.data = data as any;
